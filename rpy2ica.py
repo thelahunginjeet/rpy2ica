9acf7b6222ddf4ab93058a6e972a783b4cf7b367
Kevin Brown <kevin.s.brown@uconn.edu>
2015-06-04 11:26:55 -0400

    Test of git integration in Atom.io.

--- rpy2ica.py
+++ rpy2ica.py
@@ -1,5 +1,5 @@
 '''
-Wraps R's version of FastICA via Rpy2.  See the README for information about possible 
+Wraps R's version of FastICA via Rpy2.  See the README for information about possible
 differences between the python and R versions of FastICA.
 
 Created on June 14, 2013
@@ -11,27 +11,27 @@ This source code is provided under the BSD-3 license, duplicated as follows:
 Copyright (c) 2013, Kevin S. Brown
 All rights reserved.
 
-Redistribution and use in source and binary forms, with or without modification, 
+Redistribution and use in source and binary forms, with or without modification,
 are permitted provided that the following conditions are met:
 
-    1. Redistributions of source code must retain the above copyright notice, this 
+    1. Redistributions of source code must retain the above copyright notice, this
     list of conditions and the following disclaimer.
 
-    2. Redistributions in binary form must reproduce the above copyright notice, this 
-    list of conditions and the following disclaimer in the documentation and/or other 
+    2. Redistributions in binary form must reproduce the above copyright notice, this
+    list of conditions and the following disclaimer in the documentation and/or other
     materials provided with the distribution.
 
-    3. Neither the name of the University of Connecticut nor the names of its contributors 
-    may be used to endorse or promote products derived from this software without specific 
+    3. Neither the name of the University of Connecticut nor the names of its contributors
+    may be used to endorse or promote products derived from this software without specific
     prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS 
-OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY 
-AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
-IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 '''
 
@@ -57,15 +57,15 @@ def fastica(X,nSources,algorithm="parallel",nonlinearity="logcosh",method="C",ma
     ------------
     X : numpy array, required
         data matrix
-        
+
     nSources : integer, required
         desired number of sources to extract
 
-    algorithm : string, optional 
+    algorithm : string, optional
        "deflation" : components extracted one at a time
        "parallel"  : components extracted all at once
 
-    nonlinearity: string, optional 
+    nonlinearity: string, optional
        should be either "logcosh" or "exp"
 
     method : string, optional
@@ -82,7 +82,7 @@ def fastica(X,nSources,algorithm="parallel",nonlinearity="logcosh",method="C",ma
        initial guess for the unmixing matrix
 
     Output:
-    ---------- 
+    ----------
     A : numpy array
         mixing matix of size X.shape[0] x nSources
 
